3320a950ec800f97b5fea63beb800450c83d2307
Emilio Sánchez <esancheznumero1@gmail.com>
2020-02-02 17:00:31 +0100

    Update with further features

--- rocknpedia/src/constants.ts
+++ rocknpedia/src/constants.ts
@@ -1,3 +1,4 @@
 export const API_URL = "http://localhost:3003";
 
+
 export const defaultBandImage = "https://media.fromthegrapevine.com/assets/images/2015/8/generic_rock_concert.jpg.824x0_q71_crop-scale.jpg";
